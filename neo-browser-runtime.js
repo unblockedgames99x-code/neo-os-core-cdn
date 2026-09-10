2ffef4e9018a37fde6a97019770bb975cfd34f9d
unblockedgames99x-code <unblockedgames99x-code@users.noreply.github.com>
2026-09-09 23:17:28 -0500

    Resolve Browser proxy URLs from CDN documents

--- neo-browser-runtime.js
+++ neo-browser-runtime.js
@@ -3,7 +3,7 @@
 
   const ENGINE_VERSION = "neo-browse-v68";
   const CORE_ROOT = new URL("./", document.currentScript.src).href;
-  const BROWSER_ROOT = "https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@f50c0b914a65d5c05d1455af9cd434de0297a42d/";
+  const BROWSER_ROOT = "https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@d18d995be07032ced7eb1c82005bd9157995cdc9/";
   const OS_SCOPE = new URL("./", BROWSER_ROOT).pathname;
   const ROUTE_PREFIX = new URL("browse-v68/", BROWSER_ROOT).pathname;
   const RUNTIME_ROOT = BROWSER_ROOT + "browser-runtime";
--- neo-os.js
+++ neo-os.js
@@ -270,7 +270,7 @@
       accessibleName: "Web app",
       subtitle: "Private DuckDuckGo search",
       icon: "duckduckgo",
-    route: "https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@f50c0b914a65d5c05d1455af9cd434de0297a42d/NEO-BROWSER/launch.svg?v=20260909-worker-transport-v6",
+    route: "https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@d18d995be07032ced7eb1c82005bd9157995cdc9/NEO-BROWSER/launch.svg?v=20260909-worker-transport-v7",
       keepAlive: false,
       width: 1080,
       height: 720,
